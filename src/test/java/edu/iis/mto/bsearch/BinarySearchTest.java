6363a318391da0dddcb85b9ba3773a82c20f4334
= <=>
2020-03-24 14:24:14 +0100

    testy_v2 + poprawa kodu BinarySearch
    Signed-off-by: = <=>

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -11,52 +11,65 @@ class BinarySearchTest {
     private int[] seq2 = {1, 2, 3, 4, 5};
 
     @Test
-    void jestWSekwencji() {
+    public void chceckException_emptySequence_IllegalArgumentException() {
+        try {
+            SearchResult searchResult = BinarySearch.create().search(0, new int[0]);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void searchForExistingElem_singleElemSequence_itIsInSeq() {
         int index = 0;
+
         SearchResult searchResult = BinarySearch.create().search(4, seq1);
-        assertEquals(true, searchResult.isFound());
+        assertTrue(searchResult.isFound());
         assertEquals(4, seq1[index]);
         assertEquals(index, searchResult.getPosition());
     }
 
     @Test
-    void nieMaWSekwencji() {
+    public void searchForNonExistingElem_singleElemSequence_itIsNotInSeq() {
         SearchResult searchResult = BinarySearch.create().search(1, seq1);
-        assertEquals(false, searchResult.isFound());
+        assertFalse(searchResult.isFound());
         assertEquals(-1, searchResult.getPosition());
     }
 
     @Test
-    void jestPierwszymElementem() {
+    public void searchForExistingElem_multipleElemSequence_itIsFirst() {
         int index = 0;
+
         SearchResult searchResult = BinarySearch.create().search(1, seq2);
-        assertEquals(true, searchResult.isFound());
+        assertTrue(searchResult.isFound());
         assertEquals(1, seq2[index]);
         assertEquals(index, searchResult.getPosition());
     }
 
     @Test
-    void jestOstatnimElementem() {
+    public void searchForExistingElem_multipleElemSequence_itIsLast() {
         int index = seq2.length - 1;
+
         SearchResult searchResult = BinarySearch.create().search(5, seq2);
-        assertEquals(true, searchResult.isFound());
+        assertTrue(searchResult.isFound());
         assertEquals(5, seq2[index]);
         assertEquals(index, searchResult.getPosition());
     }
 
     @Test
-    void jestSrodkowymElementem() {
+    public void searchForExistingElem_multipleElemSequence_itIsMiddle() {
         int index = seq2.length / 2;
+
         SearchResult searchResult = BinarySearch.create().search(3, seq2);
-        assertEquals(true, searchResult.isFound());
+        assertTrue(searchResult.isFound());
         assertEquals(3, seq2[index]);
         assertEquals(index, searchResult.getPosition());
     }
 
     @Test
-    void nieMaWSekwencji2() {
+    public void searchForExistingElem_multipleElemSequence_itIsNotInSeq() {
         SearchResult searchResult = BinarySearch.create().search(8, seq2);
-        assertEquals(false, searchResult.isFound());
+        assertFalse(searchResult.isFound());
         assertEquals(-1, searchResult.getPosition());
     }
 }
