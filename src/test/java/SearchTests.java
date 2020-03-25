83b6c618f1a83cfa2900efa86cb3f1cbe0358258
Lis-Marciniak.Jędrzej <215799@edu.p.lodz.pl>
2020-03-25 16:31:52 +0100

    testy_v1 Multiple elements sequence tests

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,20 +1,20 @@
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class SearchTests {
     private BinarySearch binarySearch = BinarySearch.create();
     private int[] singleElementSequence = { 3 };
-    private int[] multipleElementSequence = { 1, 2, 3, 4, 5, 6, 7 };
+    private int[] multipleElementsSequence = { 1, 2, 3, 4, 5, 6, 7 };
 
     @Test
     public void keyInSingleElementSequenceTest() {
         int key = 3;
         SearchResult searchResult = binarySearch.search(key, singleElementSequence);
         Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(singleElementSequence[searchResult.getPosition()], key);
+        Assertions.assertEquals(0, searchResult.getPosition());
+        Assertions.assertEquals(key, singleElementSequence[searchResult.getPosition()]);
     }
 
     @Test
@@ -22,6 +22,41 @@ public class SearchTests {
         int key = 0;
         SearchResult searchResult = binarySearch.search(key, singleElementSequence);
         Assertions.assertFalse(searchResult.isFound());
-        Assertions.assertEquals(searchResult.getPosition(), -1);
+        Assertions.assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    public void keyAsFirstElementInMultipleElementsSequenceTest() {
+        int key = 1;
+        SearchResult searchResult = binarySearch.search(key, multipleElementsSequence);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(0, searchResult.getPosition());
+        Assertions.assertEquals(key, multipleElementsSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void keyAsLastElementInMultipleElementsSequenceTest() {
+        int key = 7;
+        SearchResult searchResult = binarySearch.search(key, multipleElementsSequence);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(multipleElementsSequence.length - 1, searchResult.getPosition());
+        Assertions.assertEquals(key, multipleElementsSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void keyAsMiddleElementInMultipleElementsSequenceTest() {
+        int key = 4;
+        SearchResult searchResult = binarySearch.search(key, multipleElementsSequence);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(multipleElementsSequence.length / 2, searchResult.getPosition());
+        Assertions.assertEquals(key, multipleElementsSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void keyNotInMultipleElementsSequenceTest() {
+        int key = 0;
+        SearchResult searchResult = binarySearch.search(key, multipleElementsSequence);
+        Assertions.assertFalse(searchResult.isFound());
+        Assertions.assertEquals(-1, searchResult.getPosition());
     }
 }
