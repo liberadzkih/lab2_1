bb4061bbd6c71ac281a9c8ffabf44f4511bffa1e
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-03-26 19:15:20 +0100

    testy_v1 - Tests simplification

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,17 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinarySearchTest {
 
     private BinarySearch binarySearch;
+    SearchResult searchResult;
     private int[] singleElementSequence = {1};
+    private int[] multipleElementsSequence = {1,2,3,4,5};
 
     @BeforeEach
     public void setup() {
         binarySearch = new BinarySearch();
+        searchResult = null;
     }
 
     @Test
     public void elementIsInSingleElementSequence() {
         int key = 1;
-        SearchResult searchResult;
         searchResult = binarySearch.search(key,singleElementSequence);
 
         Assertions.assertEquals(true, searchResult.isFound());
@@ -30,11 +32,11 @@ class BinarySearchTest {
     @Test
     public void elementIsNotInSingleElementSequence() {
         int key = 2;
-        SearchResult searchResult;
         searchResult = binarySearch.search(key, singleElementSequence);
 
         Assertions.assertEquals(false, searchResult.isFound());
         Assertions.assertEquals(-1, searchResult.getPosition());
 
     }
+
 }
