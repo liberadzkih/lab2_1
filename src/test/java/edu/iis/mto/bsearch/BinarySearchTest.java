264d493bcabfc08808642b415e86284672fd43f4
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-03-26 19:07:28 +0100

    testy_v1 - Single element tests

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,7 +1,40 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
 
+    private BinarySearch binarySearch;
+    private int[] singleElementSequence = {1};
+
+    @BeforeEach
+    public void setup() {
+        binarySearch = new BinarySearch();
+    }
+
+    @Test
+    public void elementIsInSingleElementSequence() {
+        int key = 1;
+        SearchResult searchResult;
+        searchResult = binarySearch.search(key,singleElementSequence);
+
+        Assertions.assertEquals(true, searchResult.isFound());
+        Assertions.assertEquals(0, searchResult.getPosition());
+        Assertions.assertEquals(key,singleElementSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void elementIsNotInSingleElementSequence() {
+        int key = 2;
+        SearchResult searchResult;
+        searchResult = binarySearch.search(key, singleElementSequence);
+
+        Assertions.assertEquals(false, searchResult.isFound());
+        Assertions.assertEquals(-1, searchResult.getPosition());
+
+    }
 }
