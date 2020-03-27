fc05e2d7234ae32488a7e3f3214aa13ea094c1fe
pawelek103 <215675@edu.p.lodz.pl>
2020-03-27 15:41:06 +0100

    testy_v1 Testowanie BinarySearch przy większej niż 1 długości sekwencji wejściowej

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,13 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinarySearchTest {
     private BinarySearch binarySearch = BinarySearch.create();
     private int[] oneElementSequence = { 1 };
+    private int[] fewElementsSequence = { 1, 2, 3, 4, 5 };
 
     @Test
     public void foundInOneElementSequenceTest() {
         int key = 1;
         SearchResult searchResult = binarySearch.search(key, oneElementSequence);
         Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(0, searchResult.getPosition());
+        Assertions.assertNotEquals(-1, searchResult.getPosition());
         Assertions.assertEquals(key, oneElementSequence[searchResult.getPosition()]);
     }
 
@@ -22,6 +23,41 @@ class BinarySearchTest {
     public void notFoundInOneElementSequenceTest() {
         int key = 0;
         SearchResult searchResult = binarySearch.search(key, oneElementSequence);
+        Assertions.assertFalse(searchResult.isFound());
+        Assertions.assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    public void foundAtFirstPositionInFewElementsSequenceTest() {
+        int key = 1;
+        SearchResult searchResult = binarySearch.search(key, fewElementsSequence);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(0, searchResult.getPosition());
+        Assertions.assertEquals(key, fewElementsSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void foundAtLastPositionInFewElementsSequenceTest() {
+        int key = 5;
+        SearchResult searchResult = binarySearch.search(key, fewElementsSequence);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(fewElementsSequence.length - 1, searchResult.getPosition());
+        Assertions.assertEquals(key, fewElementsSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void foundAtMiddlePositionInFewElementsSequenceTest() {
+        int key = 3;
+        SearchResult searchResult = binarySearch.search(key, fewElementsSequence);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(fewElementsSequence.length / 2, searchResult.getPosition());
+        Assertions.assertEquals(key, fewElementsSequence[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void notFoundInFewElementsSequenceTest() {
+        int key = 6;
+        SearchResult searchResult = binarySearch.search(key, fewElementsSequence);
         Assertions.assertFalse(searchResult.isFound());
         Assertions.assertEquals(-1, searchResult.getPosition());
     }
