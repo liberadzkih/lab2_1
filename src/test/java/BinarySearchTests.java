6b8e10b0859f56808792cc506fd45ede8d3767eb
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-03-27 19:04:19 +0100

    testy_v2 Tried functionalities of Hamcrest library.

--- src/test/java/BinarySearchTests.java
+++ src/test/java/BinarySearchTests.java
@@ -3,61 +3,64 @@ import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class BinarySearchTests {
 
-    private BinarySearch binarySearch =  BinarySearch.create();
+    private BinarySearch binarySearch = BinarySearch.create();
     final int[] emptySeq = {};
     final int[] oneElemSeq = {0};
-    final int[] multipleSeq = {0, 1, 2 ,3 , 4};
+    final int[] multipleSeq = {0, 1, 2, 3, 4};
 
     @Test
     void elementExistInSequenceTest() {
         SearchResult searchResult = binarySearch.search(0, oneElemSeq);
-        Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(0, searchResult.getPosition());
+        assertThat(true, equalTo(searchResult.isFound()));
+        assertThat(0, equalTo(searchResult.getPosition()));
     }
 
     @Test
-    void elementDoesNotExistInSequenceTest(){
+    void elementDoesNotExistInSequenceTest() {
         SearchResult searchResult = binarySearch.search(1, oneElemSeq);
-        Assertions.assertFalse(searchResult.isFound());
-        Assertions.assertEquals(-1, searchResult.getPosition());
+        assertThat(false, equalTo(searchResult.isFound()));
+        assertThat(-1, equalTo(searchResult.getPosition()));
     }
 
     @Test
-    void  elementExistOnFirstPositionTest(){
+    void elementExistOnFirstPositionTest() {
         SearchResult searchResult = binarySearch.search(0, multipleSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(0, searchResult.getPosition());
     }
 
     @Test
-    void  elementExistOnLastPositionTest(){
+    void elementExistOnLastPositionTest() {
         SearchResult searchResult = binarySearch.search(4, multipleSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(4, searchResult.getPosition());
     }
 
     @Test
-    void  elementExistOnMiddlePositionTest(){
+    void elementExistOnMiddlePositionTest() {
         SearchResult searchResult = binarySearch.search(2, multipleSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(2, searchResult.getPosition());
     }
 
     @Test
-    void  elementDoesNotExistInSequenceTest2(){
+    void elementDoesNotExistInSequenceTest2() {
         SearchResult searchResult = binarySearch.search(5, multipleSeq);
         Assertions.assertFalse(searchResult.isFound());
         Assertions.assertEquals(-1, searchResult.getPosition());
     }
 
     @Test
-    void illegalArgumentExceptionTest(){
-        try{
+    void illegalArgumentExceptionTest() {
+        try {
             binarySearch.search(0, emptySeq);
             Assertions.fail("Expected IllegalArgumentException");
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
 
         }
     }
