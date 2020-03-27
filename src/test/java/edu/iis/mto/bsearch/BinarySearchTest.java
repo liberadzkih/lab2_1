a567b60c32815b738f79c6f034b32547974adaa3
debek.kacper <215702@edu.p.lodz.pl>
2020-03-27 17:31:59 +0100

    testy_v1 Added test for element not inside when sequence length equals 1

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -14,4 +14,13 @@ class BinarySearchTest {
         assertTrue(result.isFound());
         assertEquals(0, result.getPosition());
     }
+
+    @Test void testElementIsNotInsideSequenceLength1() {
+        int testValue = 5;
+        int[] analyzedSequence = {7};
+        BinarySearch seeker = new BinarySearch();
+        SearchResult result = seeker.search(testValue, analyzedSequence);
+        assertFalse(result.isFound());
+        assertEquals(-1, result.getPosition());
+    }
 }
