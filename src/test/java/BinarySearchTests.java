ed993f6a84287482b2c3f171661a5ab399b2b343
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-03-27 17:43:50 +0100

    testy_v1 Implemented new tests for BinarySearch

--- src/test/java/BinarySearchTests.java
+++ src/test/java/BinarySearchTests.java
@@ -1,10 +1,7 @@
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 public class BinarySearchTests {
 
@@ -18,5 +15,44 @@ public class BinarySearchTests {
         Assertions.assertEquals(0, searchResult.getPosition());
     }
 
+    @Test
+    void elementDoesNotExistInSequenceTest(){
+        int[] seq = {0};
+        SearchResult searchResult = binarySearch.search(1, seq);
+        Assertions.assertFalse(searchResult.isFound());
+        Assertions.assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    void  elementExistOnFirstPositionTest(){
+        int[] seq = {0, 1, 2 ,3 , 4};
+        SearchResult searchResult = binarySearch.search(0, seq);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(0, searchResult.getPosition());
+    }
+
+    @Test
+    void  elementExistOnLastPositionTest(){
+        int[] seq = {0, 1, 2 ,3 , 4};
+        SearchResult searchResult = binarySearch.search(4, seq);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(4, searchResult.getPosition());
+    }
+
+    @Test
+    void  elementExistOnMiddlePositionTest(){
+        int[] seq = {0, 1, 2 ,3 , 4};
+        SearchResult searchResult = binarySearch.search(2, seq);
+        Assertions.assertTrue(searchResult.isFound());
+        Assertions.assertEquals(2, searchResult.getPosition());
+    }
+
+    @Test
+    void  elementDoesNotExistInSequenceTest2(){
+        int[] seq = {0, 1, 2 ,3 , 4};
+        SearchResult searchResult = binarySearch.search(5, seq);
+        Assertions.assertFalse(searchResult.isFound());
+        Assertions.assertEquals(-1, searchResult.getPosition());
+    }
 
 }
