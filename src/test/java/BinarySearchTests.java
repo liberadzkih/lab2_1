da64cb65bfc3c4de51679e7ad7ee1aca8393d3e8
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-03-27 18:36:32 +0100

    testy_v2 Added new test case and required refactoring.

--- src/test/java/BinarySearchTests.java
+++ src/test/java/BinarySearchTests.java
@@ -6,53 +6,60 @@ import org.junit.jupiter.api.Test;
 public class BinarySearchTests {
 
     private BinarySearch binarySearch =  BinarySearch.create();
+    final int[] emptySeq = {};
+    final int[] oneElemSeq = {0};
+    final int[] multipleSeq = {0, 1, 2 ,3 , 4};
 
     @Test
     void elementExistInSequenceTest() {
-        int[] seq = {0};
-        SearchResult searchResult = binarySearch.search(0, seq);
+        SearchResult searchResult = binarySearch.search(0, oneElemSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(0, searchResult.getPosition());
     }
 
     @Test
     void elementDoesNotExistInSequenceTest(){
-        int[] seq = {0};
-        SearchResult searchResult = binarySearch.search(1, seq);
+        SearchResult searchResult = binarySearch.search(1, oneElemSeq);
         Assertions.assertFalse(searchResult.isFound());
         Assertions.assertEquals(-1, searchResult.getPosition());
     }
 
     @Test
     void  elementExistOnFirstPositionTest(){
-        int[] seq = {0, 1, 2 ,3 , 4};
-        SearchResult searchResult = binarySearch.search(0, seq);
+        SearchResult searchResult = binarySearch.search(0, multipleSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(0, searchResult.getPosition());
     }
 
     @Test
     void  elementExistOnLastPositionTest(){
-        int[] seq = {0, 1, 2 ,3 , 4};
-        SearchResult searchResult = binarySearch.search(4, seq);
+        SearchResult searchResult = binarySearch.search(4, multipleSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(4, searchResult.getPosition());
     }
 
     @Test
     void  elementExistOnMiddlePositionTest(){
-        int[] seq = {0, 1, 2 ,3 , 4};
-        SearchResult searchResult = binarySearch.search(2, seq);
+        SearchResult searchResult = binarySearch.search(2, multipleSeq);
         Assertions.assertTrue(searchResult.isFound());
         Assertions.assertEquals(2, searchResult.getPosition());
     }
 
     @Test
     void  elementDoesNotExistInSequenceTest2(){
-        int[] seq = {0, 1, 2 ,3 , 4};
-        SearchResult searchResult = binarySearch.search(5, seq);
+        SearchResult searchResult = binarySearch.search(5, multipleSeq);
         Assertions.assertFalse(searchResult.isFound());
         Assertions.assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    void illegalArgumentExceptionTest(){
+        try{
+            binarySearch.search(0, emptySeq);
+            Assertions.fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e){
+
+        }
     }
 
 }
