ec0025bc718b9bbca7b7b08cb59e37c231c02381
kazie1 <kamil.zielinski@cybercom.com>
2020-03-27 21:43:38 +0100

    testy_v3 Added tests for unsorted sequence
    +Minor refactoring

--- src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
+++ src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
@@ -8,78 +8,117 @@ import org.junit.jupiter.api.Test;
 
 class SearchResultTest {
 
-    BinarySearch binarySearch = BinarySearch.create();
+    final BinarySearch BINARY_SEARCH = BinarySearch.create();
+    final int EXPECTED_FAILED_POSITION = -1;
+    final int[] SEQUENCE_WITH_ONE_ELEMENT = { 1 };
+    final int[] SEQUENCE_WITH_MULTIPLE_ELEMENTS = { 10, 20, 30, 40, 50 };
+    final int[] UNSORTED_SEQ_1 = { 60, 10, 20, 30, 40 };
+    final int[] UNSORTED_SEQ_2 = { 20, 30, 40, 10, 50 };
+    final String EXPECTED_EMPTY_OR_NULL_SEQ_EXCEPTION = "Empty or null sequence";
     SearchResult searchResult;
-    int[] emptySequence = {};
-    int[] sequenceWithOneElement = { 1 };
-    int[] sequenceWithMultipleElements = { 10, 20, 30, 40, 50 };
-    String expectedEmptyOrNullSeqException = "Empty or null sequence";
 
     @Test
     void shouldReturnTrueWhenFoundInLengthOne() {
         int searchedElement = 1;
-        searchResult = binarySearch.search(searchedElement, sequenceWithOneElement);
+        int expectedPosition = 0;
+        searchResult = BINARY_SEARCH.search(searchedElement, SEQUENCE_WITH_ONE_ELEMENT);
         assertThat(true, is(searchResult.isFound()));
-        assertThat(0, equalTo(searchResult.getPosition()));
-        assertThat(searchedElement, equalTo(sequenceWithOneElement[searchResult.getPosition()]));
+        assertThat(expectedPosition, equalTo(searchResult.getPosition()));
+        assertThat(searchedElement, equalTo(SEQUENCE_WITH_ONE_ELEMENT[searchResult.getPosition()]));
     }
 
     @Test
     void shouldReturnFalseWhenNotFoundInLengthOneSeq() {
         int searchedElement = 2;
-        searchResult = binarySearch.search(searchedElement, sequenceWithOneElement);
+        searchResult = BINARY_SEARCH.search(searchedElement, SEQUENCE_WITH_ONE_ELEMENT);
         assertThat(false, is(searchResult.isFound()));
-        assertThat(-1, equalTo(searchResult.getPosition()));
+        assertThat(EXPECTED_FAILED_POSITION, equalTo(searchResult.getPosition()));
     }
 
     @Test
     void shouldReturnFirstPositionWhenFoundInMultipleElementsSeq() {
         int searchedElement = 10;
-        searchResult = binarySearch.search(searchedElement, sequenceWithMultipleElements);
+        int expectedPosition = 0;
+        searchResult = BINARY_SEARCH.search(searchedElement, SEQUENCE_WITH_MULTIPLE_ELEMENTS);
         assertThat(true, is(searchResult.isFound()));
-        assertThat(0, equalTo(searchResult.getPosition()));
-        assertThat(searchedElement, equalTo(sequenceWithMultipleElements[searchResult.getPosition()]));
+        assertThat(expectedPosition, equalTo(searchResult.getPosition()));
+        assertThat(searchedElement, equalTo(SEQUENCE_WITH_MULTIPLE_ELEMENTS[searchResult.getPosition()]));
     }
 
     @Test
     void shouldReturnLastElementWhenFoundInMultipleElementsSeq() {
         int searchedElement = 50;
-        searchResult = binarySearch.search(searchedElement, sequenceWithMultipleElements);
+        int expectedPosition = 4;
+        searchResult = BINARY_SEARCH.search(searchedElement, SEQUENCE_WITH_MULTIPLE_ELEMENTS);
         assertThat(true, is(searchResult.isFound()));
-        assertThat(4, equalTo(searchResult.getPosition()));
-        assertThat(searchedElement, equalTo(sequenceWithMultipleElements[searchResult.getPosition()]));
+        assertThat(expectedPosition, equalTo(searchResult.getPosition()));
+        assertThat(searchedElement, equalTo(SEQUENCE_WITH_MULTIPLE_ELEMENTS[searchResult.getPosition()]));
     }
 
     @Test
     void shouldReturnMiddleElementWhenFoundInMultipleElementSeq() {
         int searchedElement = 30;
-        searchResult = binarySearch.search(searchedElement, sequenceWithMultipleElements);
+        int expectedPosition = 2;
+        searchResult = BINARY_SEARCH.search(searchedElement, SEQUENCE_WITH_MULTIPLE_ELEMENTS);
         assertThat(true, is(searchResult.isFound()));
-        assertThat(2, equalTo(searchResult.getPosition()));
-        assertThat(searchedElement, equalTo(sequenceWithMultipleElements[searchResult.getPosition()]));
+        assertThat(expectedPosition, equalTo(searchResult.getPosition()));
+        assertThat(searchedElement, equalTo(SEQUENCE_WITH_MULTIPLE_ELEMENTS[searchResult.getPosition()]));
     }
 
     @Test
     void shouldReturnFalseWhenNotFoundInMultipleElementSeq() {
         int searchedElement = 60;
-        searchResult = binarySearch.search(searchedElement, sequenceWithMultipleElements);
+        searchResult = BINARY_SEARCH.search(searchedElement, SEQUENCE_WITH_MULTIPLE_ELEMENTS);
         assertThat(false, is(searchResult.isFound()));
-        assertThat(-1, equalTo(searchResult.getPosition()));
+        assertThat(EXPECTED_FAILED_POSITION, equalTo(searchResult.getPosition()));
     }
 
     @Test
     void shouldThrowIllegalArgExceptionWhenEmptySeq() {
         int searchedElemet = 0;
+        int[] emptySequence = {};
         Throwable thrown = assertThrows(IllegalArgumentException.class,
-                () -> binarySearch.search(searchedElemet, emptySequence));
-        assertThat(expectedEmptyOrNullSeqException, equalTo(thrown.getMessage()));
+                () -> BINARY_SEARCH.search(searchedElemet, emptySequence));
+        assertThat(EXPECTED_EMPTY_OR_NULL_SEQ_EXCEPTION, equalTo(thrown.getMessage()));
     }
 
     @Test
     void shouldThrowIllegarArgrumenExcetpionWhenNullSeq() {
         int searchedElemet = 0;
         Throwable thrown = assertThrows(IllegalArgumentException.class,
-                () -> binarySearch.search(searchedElemet, null));
-        assertThat(expectedEmptyOrNullSeqException, equalTo(thrown.getMessage()));
+                () -> BINARY_SEARCH.search(searchedElemet, null));
+        assertThat(EXPECTED_EMPTY_OR_NULL_SEQ_EXCEPTION, equalTo(thrown.getMessage()));
+    }
+
+    @Test
+    void shouldReturnFalseWhenElementInUnsortedSeq1() {
+        int searchedElement = 60;
+        searchResult = BINARY_SEARCH.search(searchedElement, UNSORTED_SEQ_1);
+        assertThat(false, is(searchResult.isFound()));
+        assertThat(EXPECTED_FAILED_POSITION, equalTo(searchResult.getPosition()));
+    }
+
+    @Test
+    void shouldReturnFalseWhenElementInUnsortedSeq2() {
+        int searchedElement = 60;
+        searchResult = BINARY_SEARCH.search(searchedElement, UNSORTED_SEQ_2);
+        assertThat(false, is(searchResult.isFound()));
+        assertThat(EXPECTED_FAILED_POSITION, equalTo(searchResult.getPosition()));
+    }
+
+    @Test
+    void shouldReturnFalseWhenNoElementInUnsortedSeq1() {
+        int searchedElement = 1;
+        searchResult = BINARY_SEARCH.search(searchedElement, UNSORTED_SEQ_1);
+        assertThat(false, is(searchResult.isFound()));
+        assertThat(EXPECTED_FAILED_POSITION, equalTo(searchResult.getPosition()));
+    }
+
+    @Test
+    void shouldReturnFalseWhenNoElementInUnsortedSeq2() {
+        int searchedElement = 1;
+        searchResult = BINARY_SEARCH.search(searchedElement, UNSORTED_SEQ_2);
+        assertThat(false, is(searchResult.isFound()));
+        assertThat(EXPECTED_FAILED_POSITION, equalTo(searchResult.getPosition()));
     }
 }
