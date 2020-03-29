c2e450c51b85ffd6facfc974754633e5be087b9d
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-03-29 15:15:24 +0200

    testy_v2 refactored using Hamcrest assertThat

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,6 +3,8 @@ package edu.iis.mto.bsearch;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BinarySearchTest {
@@ -15,51 +17,51 @@ public class BinarySearchTest {
     public void should_FindElement_When_ElementIsInOneElemSequence() {
         SearchResult result = BinarySearch.create().search(1, oneElementSeq);
         assertTrue(result.isFound());
-        assertEquals(0, result.getPosition());
+        assertThat(result.getPosition(), equalTo(0));
     }
 
     @Test
     public void should_FindNoElement_When_ElementIsNotInSequence() {
         SearchResult result = BinarySearch.create().search(2, oneElementSeq);
         assertFalse(result.isFound());
-        assertEquals(-1, result.getPosition());
+        assertThat(result.getPosition(), equalTo(-1));
     }
 
     @Test
     public void should_FindElement_When_ElementIsFirstInMultiElemSequence() {
         SearchResult result = BinarySearch.create().search(1, multiElementSeq);
         assertTrue(result.isFound());
-        assertEquals(0, result.getPosition());
+        assertThat(result.getPosition(), equalTo(0));
     }
 
     @Test
     public void should_FindElement_When_ElementIsLastInMultiElemSequence() {
         SearchResult result = BinarySearch.create().search(3, multiElementSeq);
         assertTrue(result.isFound());
-        assertEquals(2, result.getPosition());
+        assertThat(result.getPosition(), equalTo(2));
     }
 
     @Test
     public void should_FindElement_When_ElementIsCentreOfMultiElemSequence() {
         SearchResult result = BinarySearch.create().search(2, multiElementSeq);
         assertTrue(result.isFound());
-        assertEquals(1, result.getPosition());
+        assertThat(result.getPosition(), equalTo(1));
     }
 
     @Test
     public void should_FindNoElement_When_ElementIsNotInMultiElemSequence() {
         SearchResult result = BinarySearch.create().search(4, multiElementSeq);
         assertFalse(result.isFound());
-        assertEquals(-1, result.getPosition());
+        assertThat(result.getPosition(), equalTo(-1));
     }
 
     @Test (expected = NullPointerException.class)
     public void should_ThrowNullPointerException_When_SequenceLenIsZero () {
-        SearchResult result = BinarySearch.create().search(1, null);
+        BinarySearch.create().search(1, null);
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void should_ThrowIllegalArgumentException_When_SequenceIsNull () {
-        SearchResult result = BinarySearch.create().search(1, emptySeq);
+        BinarySearch.create().search(1, emptySeq);
     }
 }
