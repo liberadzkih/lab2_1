4a38c8fc450f61170f8b2adbed6c095fea9ab933
Tobiasz Skorża <209458@edu.p.lodz.pl>
2020-03-29 18:31:59 +0200

    Preparing signatures of methods for futures tests

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,36 @@
+package edu.iis.mto.bsearch;
+
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTest {
+    @Test
+    public void elementIsInSequenceLengthEqualsOne() {
+
+    }
+
+    @Test
+    public void elementIsNotInSequenceLengthEqualsOne() {
+
+    }
+
+    @Test
+    public void elementIsFirstElementOfSeq() {
+
+    }
+
+    @Test
+    public void elementIsLastElementofSeq() {
+
+    }
+
+    @Test
+    public void elementIsInTheMiddleOfSeq() {
+
+    }
+
+    @Test
+    public void elementIsNotInSequenceLengthEqualsMoreThanOne() {
+
+    }
+
+}
