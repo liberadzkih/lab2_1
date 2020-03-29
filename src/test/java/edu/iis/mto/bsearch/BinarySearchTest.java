1d043368f6db282e0c102cdfefcbd0948bcf3808
Tobiasz Skorża <209458@edu.p.lodz.pl>
2020-03-29 18:43:55 +0200

    testy_v1

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,11 +1,20 @@
 package edu.iis.mto.bsearch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
+
     @Test
     public void elementIsInSequenceLengthEqualsOne() {
 
+        int[] seq = {1};
+        final int KEY = 1;
+
+        SearchResult result = BinarySearch.create().search(KEY, seq);
+
+        Assertions.assertEquals(true, result.isFound());
+        Assertions.assertEquals(0, result.getPosition());
     }
 
     @Test
