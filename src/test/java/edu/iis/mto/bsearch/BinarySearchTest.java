523a6826b538356417f3b03f9012f0da608939f9
MagdalenaDrozdz <215707@edu.p.lodz.pl>
2020-03-30 10:37:38 +0200

    testy_v1 next tests

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -16,4 +16,34 @@ class BinarySearchTest {
         assertEquals(0,searchResult.getPosition());
     }
 
+    @Test
+    void search_for_one_element_when_has_not_it_should_return_negative_value(){
+        int[] sampleSingleArray ={1};
+        BinarySearch binarySearch = new BinarySearch();
+        SearchResult searchResult=binarySearch.search(2, sampleSingleArray);
+
+        assertFalse(searchResult.isFound());
+        assertEquals(-1,searchResult.getPosition());
+    }
+
+    @Test
+    void search_for_many_elements_when_has_it_as_first_should_found_it(){
+        int[] sampleMultiArray ={1,2,3};
+        BinarySearch binarySearch = new BinarySearch();
+        SearchResult searchResult=binarySearch.search(1, sampleMultiArray);
+
+        assertTrue(searchResult.isFound());
+        assertEquals(0,searchResult.getPosition());
+    }
+
+    @Test
+    void search_for_many_elements_when_has_it_as_last_should_found_it(){
+        int[] sampleMultiArray ={1,2,3};
+        BinarySearch binarySearch = new BinarySearch();
+        SearchResult searchResult=binarySearch.search(3, sampleMultiArray);
+
+        assertTrue(searchResult.isFound());
+        assertEquals(2,searchResult.getPosition());
+    }
+
 }
