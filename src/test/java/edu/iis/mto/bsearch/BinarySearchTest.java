ee798d0e852c75e1f7c0475afaf4bb2b1754b24d
MagdalenaDrozdz <215707@edu.p.lodz.pl>
2020-03-30 10:40:55 +0200

    testy_v1 biblioteka Hamcrest

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -2,6 +2,9 @@ package edu.iis.mto.bsearch;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
@@ -40,6 +43,15 @@ class BinarySearchTest {
 
         assertTrue(searchResult.isFound());
         assertEquals(2,searchResult.getPosition());
+    }
+
+    //example with Hamcrest library
+    @Test
+    void search_for_many_elements_when_has_not_it_should_return_negative_value(){
+        SearchResult searchResult=binarySearch.search(4, sampleMultiArray);
+
+        assertTrue(searchResult.isFound());
+        assertThat(searchResult.getPosition(),is(equalTo(-1)));
     }
 
 }
