7009694423ac65ce5eef4c3201a58339b547db51
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-03-30 15:56:40 +0200

    testy_v1 element is not in single element sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTests.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTests.java
@@ -10,10 +10,19 @@ public class BinarySearchTests {
     @Test
     public void elementIsInSingleElementSequence() {
         int elementToBeFound = 5;
-        SearchResult searchResult= binarySearch.search(elementToBeFound, singleSeq);
+        SearchResult searchResult = binarySearch.search(elementToBeFound, singleSeq);
 
         Assertions.assertEquals(true, searchResult.isFound());
         Assertions.assertEquals(0, searchResult.getPosition());
         Assertions.assertEquals(elementToBeFound, singleSeq[searchResult.getPosition()]);
+    }
+
+    @Test
+    public void elementIsNotInSingleElementSequence() {
+        int elementToBeFound = 2;
+        SearchResult searchResult = binarySearch.search(elementToBeFound, singleSeq);
+
+        Assertions.assertFalse(searchResult.isFound());
+        Assertions.assertEquals(-1, searchResult.getPosition());
     }
 }
