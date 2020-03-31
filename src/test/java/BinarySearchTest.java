1c0bf58da1bd218c26342784463a0095086d38bc
Dawid <215920@edu.p.lodz.pl>
2020-03-31 15:22:40 +0200

    test_v1 tests for multiple arguments in sequence added

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -1,7 +1,11 @@
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,30 +18,90 @@ public class BinarySearchTest {
         binarySearch = BinarySearch.create();
     }
 
-    @Test
-    void shouldReturnPositiveResultForKeyInSequence() {
-        int key = 1;
-        int[] sequence = new int[]{key};
-        SearchResult searchResult = binarySearch.search(key, sequence);
+    @Nested
+    @DisplayName("Tests for single sequence")
+    class SingleSequenceTest {
+        @Test
+        void shouldReturnPositiveResultForKeyInSequence() {
+            int key = 1;
+            int[] sequence = new int[]{key};
+            SearchResult searchResult = binarySearch.search(key, sequence);
 
-        assertAll(
-                () -> assertTrue(searchResult.isFound()),
-                () -> assertEquals(0, searchResult.getPosition())
-        );
+            assertAll(
+                    () -> assertTrue(searchResult.isFound()),
+                    () -> assertEquals(0, searchResult.getPosition())
+            );
+        }
+
+        @Test
+        void shouldReturnNegativeResultForKeyNotInSingleSequence() {
+            int key = 1;
+            int[] sequence = new int[]{0};
+            SearchResult searchResult = binarySearch.search(key, sequence);
+
+            assertAll(
+                    () -> assertFalse(searchResult.isFound()),
+                    () -> assertEquals(-1, searchResult.getPosition())
+            );
+        }
     }
 
-    @Test
-    void shouldReturnNegativeResultForKeyNotInSequence() {
-        int key = 1;
-        int[] sequence = new int[]{0};
-        SearchResult searchResult = binarySearch.search(key, sequence);
+    @Nested
+    @DisplayName("Tests for sequence with length > 1")
+    class LongSequenceTest {
+        int[] sequence;
 
-        assertAll(
-                () -> assertFalse(searchResult.isFound()),
-                () -> assertEquals(-1, searchResult.getPosition())
-        );
+        @BeforeEach
+        void initializeSequence() {
+            sequence = IntStream.rangeClosed(0, 100).toArray();
+        }
+
+        @Test
+        void shouldFindKeyInTheFirstIndex() {
+            int key = sequence[0];
+            SearchResult searchResult = binarySearch.search(key, sequence);
+
+            assertAll(
+                    () -> assertTrue(searchResult.isFound()),
+                    () -> assertEquals(0, searchResult.getPosition())
+            );
+        }
+
+        @Test
+        void shouldFindKeyInTheLastIndex() {
+            int lastIndex = sequence.length - 1;
+            int key = sequence[lastIndex];
+            SearchResult searchResult = binarySearch.search(key, sequence);
+
+            assertAll(
+                    () -> assertTrue(searchResult.isFound()),
+                    () -> assertEquals(lastIndex, searchResult.getPosition())
+            );
+        }
+
+        @Test
+        void shouldFindKeyInTheMiddle() {
+            int middleIndex = sequence.length / 2;
+            int key = sequence[middleIndex];
+            SearchResult searchResult = binarySearch.search(key, sequence);
+
+            assertAll(
+                    () -> assertTrue(searchResult.isFound()),
+                    () -> assertEquals(middleIndex, searchResult.getPosition())
+            );
+
+        }
+
+        @Test
+        void shouldReturnNegativeResultForKeyNotInMultiSequence() {
+            int key = -1;
+            SearchResult searchResult = binarySearch.search(key, sequence);
+
+            assertAll(
+                    () -> assertFalse(searchResult.isFound()),
+                    () -> assertEquals(-1, searchResult.getPosition())
+            );
+        }
+
     }
-
-
-
 }
