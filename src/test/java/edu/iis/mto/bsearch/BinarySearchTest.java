a5779a48f3a10679edf71715fdab097876ae2d5f
liberadzkih <liberadzkih@gmail.com>
2020-09-12 01:55:34 +0200

    testy_v1 4rd test case

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -30,4 +30,11 @@ public class BinarySearchTest {
         assertEquals(0, searchResult.getPosition());
     }
 
+    @Test
+    public void fewElementSequence_withKeyAtLastPosition() {
+        int seq[] = {8, 9, 55, 162};
+        int key = 162;
+        SearchResult searchResult = BinarySearch.create().search(key, seq);
+        assertEquals(seq.length - 1, searchResult.getPosition());
+    }
 }
